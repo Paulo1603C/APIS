af81c9637fce7155f0ddea5849bc7fe7e736b142
Paulo Martinez <paulomartinez1999@gmail.com>
2024-08-01 11:01:25 -0500

    cambio de servidor => servidor UTA

--- Servidor.php
+++ Servidor.php
@@ -1,11 +1,11 @@
 <?php
 //Datos para el servidor 
-$servidor='192.168.5.2';
+//$servidor='192.168.5.2';
 //$servidor='10.80.7.236';
-//$servidor='172.21.123.13';
+$servidor='172.21.123.13';
 $user='acceso';
-//$pass='Fisei@2024';
-$pass='paulo243';
+$pass='Fisei@2024';
+//$pass='paulo243';
 $puerto='22';
 
 ?>
