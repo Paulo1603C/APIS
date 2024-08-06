1b73ffdbf08797a95d51f95ff22f4bcd50d0f310
Paulo Martinez <paulomartinez1999@gmail.com>
2024-08-06 14:27:57 -0500

    cambio de servisor al UTA

--- Servidor.php
+++ Servidor.php
@@ -1,11 +1,11 @@
 <?php
 //Datos para el servidor 
-$servidor='192.168.5.2';
-$pass='paulo243';
+//$servidor='192.168.5.2';
+//$pass='paulo243';
 //$servidor='10.80.7.236';
-//$servidor='172.21.123.13';
+$servidor='172.21.123.13';
 $user='acceso';
-//$pass='Fisei@2024';
+$pass='Fisei@2024';
 $puerto='22';
 
 ?>
---
+++ cragarEstudienteCarrera.php
@@ -0,0 +1,37 @@
+<?php
+
+header("Access-Control-Allow-Origin: *"); // Permite solicitudes desde tu aplicación Vue.js
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS"); // Define los métodos HTTP permitidos
+header("Access-Control-Allow-Headers: Content-Type"); // Define los encabezados permitidos
+
+require('conexion.php');
+
+$IdCar = isset($_POST['IdCar']) ? $_POST['IdCar'] : null;
+
+if ($IdCar != null) {
+    $list = array();
+    $sqlSelect = "SELECT e.IdEst, e.NomEst, e.ApeEst, e.CedEst, e.Fecha, c.IdCar, c.NomCar, u.NomUser, e.Nom_Modificador, e.IdPlanPer
+                FROM estudiantes as e
+                JOIN carreras as c ON e.IdCarPer = c.IdCar
+                JOIN carreras_secretarias as cs ON c.IdCar = cs.IdCarPer
+                JOIN usuarios as u ON cs.IdUserPer = u.IdUser
+                WHERE c.IdCar = :idCar
+                ORDER BY e.NomEst";
+    $sqlSelect = $connect->prepare($sqlSelect);
+    $sqlSelect->bindParam(':idCar', $IdCar, PDO::PARAM_INT);
+
+
+    try {
+        $sqlSelect->execute();
+        $result = $sqlSelect->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($result as $item) {
+            array_push($list, $item);
+        }
+        echo json_encode($list);
+    } catch (\Throwable $th) {
+        echo json_encode('Error en la consulta' . $th->getMessage());
+    }
+} else {
+    echo json_encode(['Message' => 'no existen datos para consulta']);
+}
+?>
